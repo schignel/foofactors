41800aa7e19a81eabe5ed29585020333134dd6bd
Steve Chignell <stephen.chignell@alumni.ubc.ca>
2018-11-12 16:40:22 -0800

    Updated example

--- R/fclean.R
+++ R/fclean.R
@@ -1,11 +1,15 @@
 #' Clean factor levels
+#'
 #' For all levels, remove white spaces and convert to title case.
 
 #' @param x factor
 #' @return factor
 #' @export
 #' @examples
+#' # Create a factor with messy levels
 #' production <- factor(c("  HIGH production", "MED production  ",  "MED production  ", " low Production"))
+#'
+#' # Run it through fclean()
 #' fclean(production)
 fclean <- function(x) {
   xtrim <- stringr::str_to_title(x)
